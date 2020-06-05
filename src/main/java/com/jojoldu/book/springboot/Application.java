80ea30c639552d7a2b63a2bec36b8726506157e5
FELIX-imbetpy-choi <imbetpy2025@naver.com>
2020-06-05 14:48:38 +0900

    finished chapter 4

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,10 +3,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
